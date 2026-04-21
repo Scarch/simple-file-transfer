33006e227d6752ecff3937668d3a22f4382834c7
Scarch <stenegert10@gmail.com>
2026-04-21 16:05:41 +0300

    Add initial foundation for FileHandler

--- inc/FileHandler.hpp
+++ inc/FileHandler.hpp
@@ -1,1 +1,34 @@
 #pragma once
+#include <filesystem>
+#include <fstream>
+#include <vector>
+
+namespace fs = std::filesystem;
+
+class FileHandler {
+public:
+
+    enum class Mode {
+        CLOSED,
+        READ,
+        WRITE
+    };
+
+    static constexpr size_t BUFFER_SIZE{8192};
+
+    explicit FileHandler(const std::string &filePath);
+
+    void openForRead();
+
+    void openForWrite();
+
+    size_t readChunk(std::vector<char> &buffer);
+
+    bool writeChunk(const std::vector<char> &buffer, size_t bytesToWrite);
+
+private:
+    fs::path m_filePath;
+    uintmax_t m_fileSize;
+    std::fstream m_fileStream{};
+    Mode m_currentMode{Mode::CLOSED};
+};
--- src/FileHandler.cpp
+++ src/FileHandler.cpp
@@ -1,1 +1,41 @@
 #include "FileHandler.hpp"
+
+#include <stdexcept>
+
+// We give m_fileSize a base value (real value depends on if we're receiving or sending a file and whether the file exists)
+FileHandler::FileHandler(const std::string &filePath) : m_filePath(filePath), m_fileSize(0) {
+}
+
+void FileHandler::openForRead() {
+    if (!fs::exists(m_filePath)) {
+        throw std::invalid_argument("File does not exist");
+    }
+
+    if (fs::is_directory(m_filePath)) {
+        throw std::invalid_argument("File cannot be a directory");
+    }
+
+    m_fileStream.open(m_filePath, std::ios::in | std::ios::binary);
+
+    if (!m_fileStream.is_open()) {
+        throw std::runtime_error("Failed to open file for reading (possible permission issue or locked file).");
+    }
+
+    m_fileSize = fs::file_size(m_filePath);
+    m_currentMode = Mode::READ;
+}
+
+void FileHandler::openForWrite() {
+    // TODO: add safe overwrite handling
+    if (fs::exists(m_filePath)) {
+        throw std::invalid_argument("File already exists");
+    }
+
+    m_fileStream.open(m_filePath, std::ios::out | std::ios::trunc | std::ios::binary);
+
+    if (!m_fileStream.is_open()) {
+        throw std::runtime_error("Failed to open file for writing (check folder permissions).");
+    }
+
+    m_currentMode = Mode::WRITE;
+}
