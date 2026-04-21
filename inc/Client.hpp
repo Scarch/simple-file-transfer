1fb92ecdb55f9ccd7f457e6da124c1928c325b45
Scarch <stenegert10@gmail.com>
2026-04-21 14:44:48 +0300

    Add initial foundation for Client

--- inc/Client.hpp
+++ inc/Client.hpp
@@ -1,1 +1,21 @@
 #pragma once
+#include <string>
+#include "FileHandler.hpp"
+#include <asio.hpp>
+using asio::ip::tcp;
+
+// Client acts as the sender
+class Client {
+public:
+    Client(const std::string &serverIp, int serverPort, asio::io_context &io_context);
+
+    void sendFile(const std::string &filePath);
+
+private:
+    asio::ip::address m_serverIp;
+    asio::ip::port_type m_serverPort;
+    asio::io_context &m_io_context;
+    tcp::socket m_socket;
+
+    bool sendMetadata();
+};
--- src/Client.cpp
+++ src/Client.cpp
@@ -1,1 +1,16 @@
 #include "Client.hpp"
+using asio::ip::tcp;
+
+Client::Client(const std::string &serverIp, int serverPort, asio::io_context &io_context) : m_serverIp(asio::ip::make_address(serverIp)),
+    m_serverPort(serverPort), m_io_context(io_context),
+    m_socket(m_io_context) {
+}
+
+void Client::sendFile(const std::string &filePath) {
+
+    const tcp::endpoint endpoint{m_serverIp, m_serverPort};
+    m_socket.connect(endpoint);
+
+    
+
+}
