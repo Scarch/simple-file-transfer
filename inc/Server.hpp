1212b6ea159c1c34b428919b8dc5e3c30daf62fd
Scarch <stenegert10@gmail.com>
2026-04-22 13:43:39 +0300

    Enhance Server class constructor and receiveMetadata function

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -2,17 +2,23 @@
 #include <string>
 #include "FileHandler.hpp"
 #include <asio.hpp>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 using asio::ip::tcp;
 
 // Server acts as the receiver
 class Server {
 public:
-    Server(int port, asio::io_context &io_context);
+    Server(const std::string &outputDirectory, int port, asio::io_context &io_context);
+
+    void start();
 
 private:
-    asio::ip::port_type m_serverPort;
+    asio::ip::port_type m_port;
     asio::io_context &m_io_context;
     tcp::acceptor m_acceptor;
+    fs::path m_outputDirectory;
 
-    FileHandler receiveMetadata();
+    FileHandler receiveMetadata(tcp::socket& socket);
 };
--- src/Server.cpp
+++ src/Server.cpp
@@ -1,1 +1,18 @@
 #include "Server.hpp"
+
+#include <stdexcept>
+
+
+Server::Server(const std::string &outputDirectory, int port, asio::io_context &io_context) : m_port(port), m_io_context(io_context), m_acceptor(tcp::acceptor(
+        m_io_context, tcp::endpoint(tcp::v4(), m_port))),
+                                                         m_outputDirectory(outputDirectory) {
+    if (fs::exists(m_outputDirectory) && !fs::is_directory(m_outputDirectory)) {
+        throw std::invalid_argument("Folder destination is occupied with non-directory element");
+    }
+}
+
+FileHandler Server::receiveMetadata(tcp::socket& socket) {
+
+    FileMetadata receivedMetadata{"", 0};
+
+}
