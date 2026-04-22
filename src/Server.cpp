4c4370ea72a6b6aa5177c22d49738550179dd71b
Scarch <stenegert10@gmail.com>
2026-04-22 13:01:21 +0300

    Add foundation for Server

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,1 +1,18 @@
 #include "Server.hpp"
+#include <string>
+#include "FileHandler.hpp"
+#include <asio.hpp>
+using asio::ip::tcp;
+
+// Server acts as the receiver
+class Server {
+public:
+    Server(int port, asio::io_context &io_context);
+
+private:
+    asio::ip::port_type m_serverPort;
+    asio::io_context &m_io_context;
+    tcp::acceptor m_acceptor;
+
+    FileHandler receiveMetadata();
+};
