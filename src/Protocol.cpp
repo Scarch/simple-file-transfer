45ededea77716b8d786391cb44c58dd830939cd2
Scarch <stenegert10@gmail.com>
2026-04-22 15:08:29 +0300

    Format Protocol files

--- src/Protocol.cpp
+++ src/Protocol.cpp
@@ -5,8 +5,8 @@ inline uint64_t swapBytes64(uint64_t value) {
     return ((value & 0x00000000000000FFULL) << 56) |
            ((value & 0x000000000000FF00ULL) << 40) |
            ((value & 0x0000000000FF0000ULL) << 24) |
-           ((value & 0x00000000FF000000ULL) <<  8) |
-           ((value & 0x000000FF00000000ULL) >>  8) |
+           ((value & 0x00000000FF000000ULL) << 8) |
+           ((value & 0x000000FF00000000ULL) >> 8) |
            ((value & 0x0000FF0000000000ULL) >> 24) |
            ((value & 0x00FF000000000000ULL) >> 40) |
            ((value & 0xFF00000000000000ULL) >> 56);
